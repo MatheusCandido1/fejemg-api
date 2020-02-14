eaf9a9f1275f062c9b3d154956fe4593ef679c9b
Matheus Carvalho <matheus9bh@hotmail.com>
2020-02-14 20:27:37 -0300

    add-current-nps

--- database/migrations/2020_02_14_232643_add_current_nps_table_junior_enterprise_goals.php
+++ database/migrations/2020_02_14_232643_add_current_nps_table_junior_enterprise_goals.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddMembersPerformingTableJuniorEnterpriseGoals extends Migration
+class AddCurrentNpsTableJuniorEnterpriseGoals extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddMembersPerformingTableJuniorEnterpriseGoals extends Migration
     public function up()
     {
         Schema::table('junior_enteprise_goals', function (Blueprint $table) {
-            $table->integer('members_performing_goal');
+            $table->integer('current_nps');
         });
     }
 
@@ -26,7 +26,7 @@ class AddMembersPerformingTableJuniorEnterpriseGoals extends Migration
     public function down()
     {
         Schema::table('junior_enteprise_goals', function (Blueprint $table) {
-            $table->dropColumn('members_performing_goal');
+            $table->dropColumn('current_nps');
         });
     }
 }
